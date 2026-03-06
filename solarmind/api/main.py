5b31ce1246f5dfd0c012658b3d6027e1339fdb65
gopu07 <devrajsolanki0704@gmail.com>
2026-03-06 21:41:42 +0530

    Fix CORS policy to allow frontend access with credentials

--- solarmind/api/main.py
+++ solarmind/api/main.py
@@ -86,7 +86,12 @@ app.mount("/metrics", metrics_app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
